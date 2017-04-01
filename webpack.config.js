3d093f74dbf46819b6b173fd0546ce35acac2922
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-04-01 14:18:34 +0100

    Use rules instead of loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,20 +22,32 @@ const commonConfig = merge([
     },
     devtool: 'source-map',
     module: {
-      loaders: [
+      rules: [
         {
           test: /\.ts$/,
-          loader: 'ts-loader'
+          use: [{
+            loader: 'ts-loader'
+
+          }]
         },
         {
           test: /\.p?css$/,
-          loaders: [
-            'style-loader',
-            'css-loader?importLoaders=1,url=false',
-            'postcss-loader'
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1, url: false
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
           ]
         }
-      ]
+      ],
     },
 
     resolve: {
